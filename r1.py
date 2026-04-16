9eba716fb021b78aef19f9c162f7f64cecc8bcd1
dhen25 <domhen1@icloud.com>
2026-04-17 00:52:56 +0100

    getting somewhere??

--- r1.py
+++ r1.py
@@ -1,6 +1,8 @@
+#from datamodel import OrderDepth, UserID, TradingState, Order
 import numpy as np
 from collections import deque
 import pandas as pd
+import matplotlib.pyplot as plt
 
 prices = ["prices0.csv", "prices1.csv", "prices2.csv"]
 trades = ["trades0.csv", "trades1.csv", "trades2.csv"]
@@ -11,9 +13,20 @@ class Engine():
         self.prices = prices
     
     def get_data(self) -> None:
-        df = pd.read_csv(self.prices[0], sep=';')
-        print(df)
+        self.df = pd.read_csv(self.prices[0], sep=';')
+        print(self.df.describe())
+    
+    def show_price_history(self) -> None:
+        self.df[self.df["product"] == "INTARIAN_PEPPER_ROOT"].plot(x="timestamp", y="mid_price")
+        self.df[self.df["product"] == "ASH_COATED_OSMIUM"].plot(x="timestamp", y="mid_price")
+    
+    def get_best_bid_ask(self) -> tuple[int, int]:
+        best_bid = best_ask = None
+
 
 engine = Engine("Engine", prices)
 engine.get_data()
+engine.show_price_history()
+plt.show()
+
 
