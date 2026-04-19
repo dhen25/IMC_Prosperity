c902a93fd3ef4d90caa2587eefbd1e3b54e50c03
dhen25 <domhen1@icloud.com>
2026-04-19 14:53:16 +0100

    r2 drafts

--- R2/src/main.py
+++ R2/src/main.py
@@ -1,1 +1,24 @@
-ls
+#from datamodel import OrderDepth, TradingState, Order
+import pandas as pd
+import matplotlib.pyplot
+import numpy as np
+
+prices = ["../test/prices0.csv", "../test/prices1.csv", "../test/prices2.csv"]
+trades = ["..test/trades0.csv", "../test/trades1.csv", "../test/trades2.csv"]
+
+class Trader:
+
+    def __init__(self, prices, trades):
+        self.prices = prices
+        self.trades = trades
+    
+    def run(self):
+        result = {}
+
+    def bid(self):
+        pass
+
+    def show_trend(self):
+        self.df = pd.read_csv(self.prices[0], sep=";")
+
+    
